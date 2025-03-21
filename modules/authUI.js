e5f5a68a424dd982489bc1300ff18908b5635738
VolPopov <volodjapopov01@gmail.com>
2025-03-21 10:03:57 +0100

    Started adding regression test for login and register

--- modules/authUI.js
+++ modules/authUI.js
@@ -2,6 +2,7 @@ import { expect } from "@playwright/test";
 import { VALID_USER_CREDENTIALS } from "../fixtures/credentials.js";
 import { URLS } from "../fixtures/urls.js";
 import { generateUserCredentials } from "../generalFunctions/functions.js";
+import { ERROR_MESSAGES } from "../fixtures/messages.js";
 
 const { username1, email1, password1 } = generateUserCredentials(5);
 
@@ -12,11 +13,15 @@ export class AuthUI {
         this.email = page.locator('#email');
         this.password = page.locator('#password');
         this.submitButton = page.locator('button');
+        this.h1Banner = page.locator('h1');
+        this.loginErrorMessage = page.locator('p');
       }
     
       async login({
         email = VALID_USER_CREDENTIALS["VALID_EMAIL"], 
-        password = VALID_USER_CREDENTIALS["VALID_PASSWORD"]
+        password = VALID_USER_CREDENTIALS["VALID_PASSWORD"], 
+        statusCode = 200, 
+        message, 
       }) {
         expect(this.email).toBeEditable();
         await this.email.fill(email);
@@ -26,14 +31,55 @@ export class AuthUI {
         const responsePromise = this.page.waitForResponse("/api/v1/auth/login");
         await this.submitButton.click();
         const response = await responsePromise;
-        expect(response.status()).toBe(200);
+        expect(response.status()).toBe(statusCode);
+        if (response.status() == 200) { 
         await expect(this.page).toHaveURL(URLS["DASHBOARD"]);
+        }
+
+        if(response.status() == 401) {
+          let responseJSON = await response.json();
+          console.log(responseJSON);
+          expect(responseJSON).toEqual({
+            error: expect.any(String), 
+          });
+          expect(responseJSON.error).toBe(ERROR_MESSAGES["UNAUTHORIZED"]);
+          await expect(this.page).toHaveURL(URLS["LOGIN_PAGE"]);
+          await expect(this.h1Banner).toBeVisible();
+          await expect(this.loginErrorMessage).toBeVisible();
+          await expect(this.loginErrorMessage).toContainText(ERROR_MESSAGES["EMAIL_OR_PASS_INVALID"]);
+        }
+
+        if (response.status() == 422) {
+
+          let responseJSON = await response.json();
+          console.log(responseJSON);
+          expect(responseJSON).toEqual({
+          message: expect.any(String), 
+          errors: expect.any(Object), 
+          });
+          expect(responseJSON.message).toBe(message);
+          await expect(this.page).toHaveURL(URLS["LOGIN_PAGE"]);
+          await expect(this.h1Banner).toBeVisible();
+          if (Object.keys(responseJSON.errors).length == 2) {
+            await expect(this.loginErrorMessage.nth(0)).toBeVisible();
+            await expect(this.loginErrorMessage.nth(0)).toContainText(ERROR_MESSAGES["EMAIL_MISSING"]);
+            await expect(this.loginErrorMessage.nth(1)).toBeVisible();
+            await expect(this.loginErrorMessage.nth(1)).toContainText(ERROR_MESSAGES["PASSWORD_MISSING"]);
+          }
+          else {
+            await expect(this.loginErrorMessage).toBeVisible();
+            await expect(this.loginErrorMessage).toContainText(message);
+          }
+        }
       }
 
       async register({
         username = username1, 
         email = email1, 
-        password = password1
+        password = password1, 
+        statusCode = 200, 
+        message, 
+        error, 
       }) {
         expect(this.username).toBeEditable();
         await this.username.fill(username);
@@ -45,7 +91,43 @@ export class AuthUI {
         const responsePromise = this.page.waitForResponse("/api/v1/auth/register");
         await this.submitButton.click();
         const response = await responsePromise;
-        expect(response.status()).toBe(200);
-        await expect(this.page).toHaveURL(URLS["DASHBOARD"]);
+        expect(response.status()).toBe(statusCode);
+
+        if (response.status() == 200) {
+          let responseJSON = await response.json();
+          console.log(responseJSON);
+          
+          await expect(this.page).toHaveURL(URLS["DASHBOARD"]);
+        }
+
+        if(response.status() == 422) {
+          let responseJSON = await response.json();
+          console.log(responseJSON);
+          expect(responseJSON).toEqual({
+            message: expect.any(String), 
+            errors: expect.any(Object), 
+          });
+          expect(responseJSON.message).toBe(message);
+          expect(this.page).toHaveURL(URLS["REGISTER_PAGE"]);
+          expect(this.h1Banner).toBeVisible();
+          if(Object.keys(responseJSON.errors).length == 3) {
+            expect(this.loginErrorMessage.nth(0)).toBeVisible();
+            expect(this.loginErrorMessage.nth(0)).toContainText(ERROR_MESSAGES["USERNAME_MISSING"]);
+            expect(this.loginErrorMessage.nth(1)).toBeVisible();
+            expect(this.loginErrorMessage.nth(1)).toContainText(ERROR_MESSAGES["EMAIL_MISSING"]);
+            expect(this.loginErrorMessage.nth(2)).toBeVisible();
+            expect(this.loginErrorMessage.nth(2)).toContainText(ERROR_MESSAGES["PASSWORD_MISSING"]);
+          }
+          if(Object.keys(responseJSON.errors).length == 2 ) {
+            expect(this.loginErrorMessage.nth(0)).toBeVisible();
+            expect(this.loginErrorMessage.nth(1)).toBeVisible();
+          }
+
+          if(Object.keys(responseJSON.errors).length == 1 ) {
+            expect(this.loginErrorMessage).toBeVisible();
+            expect(this.loginErrorMessage).toContainText(message);
+          }
+          
+        }
       }
 }
--- tests/registerUI.spec.js
+++ tests/registerUI.spec.js
@@ -1,10 +1,27 @@
 import { test } from "../fixtures/basePage.js";
 import { URLS } from "../fixtures/urls.js";
+import { ERROR_MESSAGES } from "../fixtures/messages.js";
 
 test.describe('Register tests', () => {
 
   test.beforeEach('Visit the login page', async ({ page }) => {
     await page.goto(URLS["REGISTER_PAGE"]);
+  });
+
+  test("Attempt to register user with no credentials at all", { tag: "@regression" }, async ({ authUI }) => {
+    await authUI.register({ username: "", email: "", password: "", statusCode: 422, message: ERROR_MESSAGES["USERNAME_EMAIL_AND_PASSWORD_ALL_MISSING"] });
+  });
+
+  test("Attempt to register user with no username", { tag: "@regression" }, async({ authUI }) => {
+    await authUI.register({ username: "", statusCode: 422, message: ERROR_MESSAGES["USERNAME_MISSING"] });
+  });
+
+  test("Attempt to register with no email", { tag: "@regression" }, async ({ authUI }) => {
+    await authUI.register({ email: "", statusCode: 422, message: ERROR_MESSAGES["EMAIL_MISSING"] });
+  });
+
+  test("Attempt to register with no password", { tag: "@regression" }, async ({ authUI }) => {
+    await authUI.register({ password: "", statusCode: 422, message: ERROR_MESSAGES["PASSWORD_MISSING"] });
   });
 
   test("Register", { tag: "@smoke" }, async ({ authUI }) => {
