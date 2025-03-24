cbed443df11017efef6a769dae27eb6f05c86c66
VolPopov <volodjapopov01@gmail.com>
2025-03-24 12:30:36 +0100

    Made some changes to the structure of functions

--- modules/authUI.js
+++ modules/authUI.js
@@ -15,15 +15,22 @@ export class AuthUI {
         this.submitButton = page.locator('button');
         this.h1Banner = page.locator('h1');
         this.loginErrorMessage = page.locator('p');
+        this.headerAfterLogin = page.locator(
+          "div[class='flex w-32 h-12 align-items-center']"
+        );
+        this.button = this.headerAfterLogin.locator('button');
+        this.cartButton = this.button.nth(0);
+        this.logoutButton = this.button.nth(1);
       }
     
       async login({
         email = VALID_USER_CREDENTIALS["VALID_EMAIL"], 
         password = VALID_USER_CREDENTIALS["VALID_PASSWORD"], 
         statusCode = 200, 
-        message, 
-        error, 
+        message,  
       }) {
+        expect(this.h1Banner).toBeVisible();
+        expect(this.h1Banner).toContainText("Welcome Back! 👋🏻");
         expect(this.email).toBeEditable();
         await this.email.fill(email);
         expect(this.password).toBeEditable();
@@ -33,43 +40,34 @@ export class AuthUI {
         await this.submitButton.click();
         const response = await responsePromise;
         expect(response.status()).toBe(statusCode);
+
         if (response.status() == 200) {     
         await expect(this.page).toHaveURL(URLS["DASHBOARD"]);
+        await expect(this.cartButton).toBeEnabled();
+        await expect(this.logoutButton).toBeEnabled();
         }
 
-        if(response.status() == 401) {
-          let responseJSON = await response.json();
-          console.log(responseJSON);
-          expect(responseJSON).toEqual({
-            error: expect.any(String), 
-          });
-          expect(responseJSON.error).toBe(error);
+        if(response.status() != 200) {     
+          let responseJSON = await response.json();   
           await expect(this.page).toHaveURL(URLS["LOGIN_PAGE"]);
           await expect(this.h1Banner).toBeVisible();
+
+          if(response.status() == 401) {
           await expect(this.loginErrorMessage).toBeVisible();
           await expect(this.loginErrorMessage).toContainText(ERROR_MESSAGES["EMAIL_OR_PASS_INVALID"]);
-        }
-
-        if (response.status() == 422) {
-
-          let responseJSON = await response.json();
-          console.log(responseJSON);
-          expect(responseJSON).toEqual({
-          message: expect.any(String), 
-          errors: expect.any(Object), 
-          });
-          expect(responseJSON.message).toBe(message);
-          await expect(this.page).toHaveURL(URLS["LOGIN_PAGE"]);
-          await expect(this.h1Banner).toBeVisible();
-          if (Object.keys(responseJSON.errors).length == 2) {
-            await expect(this.loginErrorMessage.nth(0)).toBeVisible();
-            await expect(this.loginErrorMessage.nth(0)).toContainText(ERROR_MESSAGES["EMAIL_MISSING"]);
-            await expect(this.loginErrorMessage.nth(1)).toBeVisible();
-            await expect(this.loginErrorMessage.nth(1)).toContainText(ERROR_MESSAGES["PASSWORD_MISSING"]);
           }
-          else {
-            await expect(this.loginErrorMessage).toBeVisible();
-            await expect(this.loginErrorMessage).toContainText(message);
+
+          if(response.status() == 422) {
+            if (Object.keys(responseJSON.errors).length == 2) {
+              await expect(this.loginErrorMessage.nth(0)).toBeVisible();
+              await expect(this.loginErrorMessage.nth(0)).toContainText(ERROR_MESSAGES["EMAIL_MISSING"]);
+              await expect(this.loginErrorMessage.nth(1)).toBeVisible();
+              await expect(this.loginErrorMessage.nth(1)).toContainText(ERROR_MESSAGES["PASSWORD_MISSING"]);
+            }
+            else {
+              await expect(this.loginErrorMessage).toBeVisible();
+              await expect(this.loginErrorMessage).toContainText(message);
+            }
           }
         }
       }
@@ -80,8 +78,9 @@ export class AuthUI {
         password = password1, 
         statusCode = 200, 
         message, 
-        error, 
       }) {
+        expect(this.h1Banner).toBeVisible();
+        expect(this.h1Banner).toContainText("Register!");
         expect(this.username).toBeEditable();
         await this.username.fill(username);
         expect(this.email).toBeEditable();
@@ -93,25 +92,22 @@ export class AuthUI {
         await this.submitButton.click();
         const response = await responsePromise;
         expect(response.status()).toBe(statusCode);
-        let responseJSON;
+        let responseJSON = await response.json();
+        
 
         if (response.status() == 200) {
-          responseJSON = await response.json();
-          console.log(responseJSON);
-          
           await expect(this.page).toHaveURL(URLS["DASHBOARD"]);
+          await expect(this.cartButton).toBeEnabled();
+          await expect(this.logoutButton).toBeEnabled();
         }
 
-        if(response.status() == 422) {
+        if(response.status() != 200) {
           responseJSON = await response.json();
-          console.log(responseJSON);
-          expect(responseJSON).toEqual({
-            message: expect.any(String), 
-            errors: expect.any(Object), 
-          });
-          expect(responseJSON.message).toBe(message);
           expect(this.page).toHaveURL(URLS["REGISTER_PAGE"]);
           expect(this.h1Banner).toBeVisible();
+
+          if(response.status() == 422) {
+                   
           if(Object.keys(responseJSON.errors).length == 3) {
             expect(this.loginErrorMessage.nth(0)).toBeVisible();
             expect(this.loginErrorMessage.nth(0)).toContainText(ERROR_MESSAGES["USERNAME_MISSING"]);
@@ -130,6 +126,7 @@ export class AuthUI {
             expect(this.loginErrorMessage).toContainText(message);
           }
         }
+      }
         return responseJSON;
       }
 }
